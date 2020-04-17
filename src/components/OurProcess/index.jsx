c58387d7118f9381729a3d958ed62a4493f2a8a6
Jesus Aguiar <aguiarf.j16@gmail.com>
2020-04-17 02:28:25 -0300

    formatted texts

--- src/components/OurProcess/index.jsx
+++ src/components/OurProcess/index.jsx
@@ -24,19 +24,19 @@ const OurProcess = () => {
             <p>
               Se hacen a partir de una estructura de álamo carolino la cual es
               una madera super dura. Se hacen de pulgadas y bulones, no grampas
-              no clavos. <br />
+              ni clavos. <br />
               <br />
-              La estructura va toda cubierta con una plancha de 5 cms de alta
-              densidad. Llevan resortes nosar ( resorte de auto) y cinchas
+              La estructura va toda cubierta con una plancha de 5cm de alta
+              densidad. Llevan resortes nosar (resorte de auto) y cinchas
               italianas a lo largo y ancho de estructura. <br />
               <br />
-              Los almohadones de asiento se hacen con plnchas de soft de alta
-              densidad ( puede ser optativo densidad 26 , 28 y 32) recubierto
+              Los almohadones de asiento se hacen con planchas de soft de alta
+              densidad (puede ser optativa la densidad con opciones de 26, 28 y 32), recubierto
               con guata en manta para sacar rigidez de la plancha y dar redondeo
               en su forma. <br />
               <br />
               Los almohadones de respaldo se realizan con guata siliconada para
-              un almohadon mas descontracturado y si no de plancha y guata para
+              un almohadon mas descontracturado y si no, de plancha y guata para
               un almohadon mas armado a la vista.
             </p>
           </ProccessTextDiv>
@@ -48,23 +48,23 @@ const OurProcess = () => {
             <h1>Retapizado</h1>
             <p>
               Lo importante en un retapizado es distinguir si vale la pena o no
-              invertir en un mueble usado. Cuando el sofa fue bien armado desde
+              invertir en un mueble usado. Cuando el sofa fué bien armado desde
               el origen y con buenos materiales siempre vale la pena invertir en
               retapizar. <br />
               <br />
-              Como nos damos cuenta si fue bueno armado en origen? por lo
-              general su estructura no tiene movimientos y sus materiales aunque
-              hayan perdido densidad siguen estando en uso.
+              ¿Como nos damos cuenta si fue bueno armado en origen? Por lo
+              general su estructura no tiene movimientos y sus materiales, aunque
+              hayan perdido densidad, siguen estando en uso.
               <br />
               <br />
-              Lo que no sirve retapizar es el tipo de muebles que fue armado de
-              una manera mas standar. Nuestro retapizados se desraman por
+              Lo que no sirve retapizar es el tipo de muebles que fué armado de
+              una manera mas estándar. Nuestros retapizados se desraman por
               completo, no se forran encima, desarmamos y volvemos a armar con
               materiales nuevos y de muy buena calidad.
               <br />
-              <br /> Tenemos la obligacion que si hacemos gastar a un cliente en
+              <br /> Tenemos la obligación de que si hacemos gastar a un cliente en
               arreglar un mueble y gaste en una muy buena tela o cuero, la
-              estructura y armado debe acompanar la duracion de dichos
+              estructura y armado debe acompañar la duración de dichos
               materiales.
             </p>
           </ProccessTextDiv>
